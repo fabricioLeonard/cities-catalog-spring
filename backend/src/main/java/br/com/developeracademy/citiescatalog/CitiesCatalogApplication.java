47ea10a52fe947b5d8c328798c3c4561a32c7087
Fabricio Leonard Leopoldino <fabricioleonard@gmail.com>
2021-08-09 22:56:34 -0300

    Backend project Cities Catalog created

---
+++ backend/src/main/java/br/com/developeracademy/citiescatalog/CitiesCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.developeracademy.citiescatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/developeracademy/citiescatalog/CitiesCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.developeracademy.citiescatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
